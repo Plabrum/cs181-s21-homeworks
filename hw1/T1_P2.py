455e163420c8ed0f84f479d8b95bfe1890364473
Plabrum <plabrum@college.harvard.edu>
2021-02-04 10:20:17 -0500

    completed question 2

--- hw1/T1_P2.py
+++ hw1/T1_P2.py
@@ -35,6 +35,31 @@ def predict_knn(k=1):
     # TODO: your code here
     return y_df
 
+
+def predict_kernel(alpha=0.1):
+    """Returns predictions using kernel-based predictor with the specified alpha."""
+    y_pred_list = []
+    
+    for value in df.itertuples():
+        x = np.array([value[1], value[2]])
+        alt_data = list(df[["x1", "x2", "y"]][:value[0]].append( df[["x1", "x2","y"]][value[0]+1:], ignore_index=True).to_records(index=False))
+        y_pred_list.append(kernelized_regression(alpha, x, alt_data))
+        
+    return pd.Series(y_pred_list)
+
+def predict_knn(k=1):
+    """Returns predictions using KNN predictor with the specified k."""
+    # TODO: your code here
+    y_pred_list = []
+    
+    for value in df.itertuples():
+        x = np.array([value[1], value[2]])
+        alt_data = list(df[["x1", "x2", "y"]][:value[0]].append( df[["x1", "x2","y"]][value[0]+1:], ignore_index=True).to_records(index=False))
+        y_pred_list.append(knn_regression(k, x, alt_data))
+        
+    return pd.Series(y_pred_list)
+
+
 def plot_kernel_preds(alpha):
     title = 'Kernel Predictions with alpha = ' + str(alpha)
     plt.figure()
@@ -87,6 +112,41 @@ def plot_knn_preds(k):
     # Saving the image to a file, and showing it as well
     plt.savefig('k' + str(k) + '.png')
     plt.show()
+
+def knn_regression(k, test_x, alt_data):
+    W = alpha * np.array([[1., 0.], [0., 1.]])
+    def kernel(x, x_prime, W):
+        z = x - x_prime
+        return np.exp(-1*((z.T)@(W@z)))
+    
+    # construct an ordered of distance and y values list
+    nearest_list = []
+    for value in alt_data:
+        distance = kernel(test_x, np.array([value[0], value[1]]), W)
+        nearest_list.append((distance, value[2]))
+
+    # sort the list
+    nearest_list.sort(key=(lambda fs: fs[0]), reverse=True)
+    # take only the top k values
+    k_nearest = nearest_list[:k]
+    y_hat = (1/k)*sum([y for _,y in k_nearest])
+    return y_hat
+
+def kernelized_regression(alpha, test_x, alt_data):
+    W = alpha * np.array([[1., 0.], [0., 1.]])
+    def kernel(x, x_prime, W):
+        z = x - x_prime
+        return np.exp(-1*((z.T)@(W@z)))
+    
+    top_list =[]
+    bottom_list = []
+    for value in alt_data:
+        distance = kernel(test_x, np.array([value[0], value[1]]), W)
+        top_list.append(distance*value[2])
+        bottom_list.append(distance)
+
+    y_hat = sum(top_list)/sum(bottom_list)
+    return y_hat
 
 for alpha in (0.1, 3, 10):
     # TODO: Print the loss for each chart.
