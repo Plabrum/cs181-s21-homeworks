bf8b996c4a02918535aa9bb3320975f745affe24
Plabrum <plabrum@college.harvard.edu>
2021-03-19 15:19:15 -0400

    p2.1 complete

--- hw4/T4_P2.py
+++ hw4/T4_P2.py
@@ -3,11 +3,16 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style(style="whitegrid")
 
 # This line loads the images for you. Don't change it!
-pics = np.load("data/images.npy", allow_pickle=False)
+# pics = np.load("data/images.npy", allow_pickle=False)
+small_dataset = np.load("data/small_dataset.npy")
+# small_labels = np.load("../data/small_dataset_labels.npy").astype(int)
+large_dataset = np.load("data/large_dataset.npy")
 
-print(pics.shape)
+print(large_dataset.shape)
 
 # You are welcome to change anything below this line. This is just an example of how your code may look.
 # Keep in mind you may add more public methods for things like the visualization.
@@ -17,25 +22,115 @@ class KMeans(object):
     # K is the K in KMeans
     def __init__(self, K):
         self.K = K
+    
+    def __make_one_hot(self, array):
+        one_hot = np.zeros(array.shape)
+        min_pos = np.array([np.argmin(row) for row in array])
+        one_hot[np.arange(array.shape[0]), min_pos] = 1
+        return one_hot 
 
+    def __distance(self, p1, p2):
+        # calulate the l2 distance between points, i.e. euclidean
+        # each is a 1xD matrix
+        return sum(pow(p1-p2, 2))
+       
     # X is a (N x 28 x 28) array where 28x28 is the dimensions of each of the N images.
     def fit(self, X):
-        pass
+        # 1 Find the cluster centers (cluster mean)
+        def cluster_mean(resp, data):
+            # for each datapoint in cluster c,
+            # sum it's vals and divide by number in class
+            class_sums = np.zeros((resp.shape[1], data.shape[1]))
+            for point, res in zip(data, resp):
+                class_sums[np.argmax(res)] += point
+            # count up the number of datapoints in each class    
+            class_counts = np.sum(resp, axis=0)
+            
+            # class_mean = np.divide(class_sums, class_counts)
+            class_mean = []
+            for class_sum, class_count in zip(class_sums, class_counts):
+                class_mean.append(np.divide(class_sum, class_count))
+                
+            class_mean = np.array(class_mean)
+            return class_mean 
+
+        # 2 adjust reponsibility vectors
+        def adjust_resp(mu, data):
+            # resp dims: NxC
+            # mu dims:
+            positions = []
+            for point in data:
+                distances = []
+                for class_mean in mu:
+                    distances.append(self.__distance(class_mean, point))
+                one_ht = np.zeros(mu.shape[0])
+                one_ht[np.argmin(distances)] = 1
+                positions.append(one_ht)
+                
+            return np.array(positions)
+
+        # 3 calculate the loss
+        def loss_func(data, mu, resp):
+            loss_list = []
+            # Loop through each datapoint in the dataset
+            for datapoint, res in zip(data, resp):
+                cluster = np.argmax(res)
+                loss_list.append(self.__distance(datapoint, mu[cluster]))
+            return sum(loss_list)
+
+        # initialisation of cluster asignment i.e. responsibility vectors
+        self.resp = self.__make_one_hot(np.random.randn(X.shape[0], self.K))
+        # Set inital mu
+        self.mu = np.random.randn(K, X.shape[1])
+        self.loss_list = []
+
+        # record the losses at each epoch
+        converge = False
+        epoch = 0
+        while not converge:
+            # 1 calc the mean of the clusters
+            self.mu = cluster_mean(self.resp, X)
+            
+            # 2 Calc the loss
+            current_loss = loss_func(X, self.mu, self.resp)
+
+            # 3 adjust the responsibility vectors
+            new_resp = adjust_resp(self.mu, X)
+            if np.array_equal(new_resp, self.resp):
+                converge = True
+            self.resp = new_resp
+            epoch += 1
+            self.loss_list.append(current_loss)
+            print("Epoch:", epoch)
+
 
     # This should return the arrays for K images. Each image should represent the mean of each of the fitted clusters.
     def get_mean_images(self):
-        pass
+        return self.mu
+
 
 K = 10
-KMeansClassifier = KMeans(K=10, useKMeansPP=False)
-KMeansClassifier.fit(pics)
+KMeansClassifier = KMeans(K=10)
+KMeansClassifier.fit(large_dataset)
+
+fig, ax = plt.subplots()
+ax.plot(np.array(KMeansClassifier.loss_list));
+ax.set_ylabel('Loss', fontsize=15)
+ax.set_xlabel('Iterations', fontsize=15)
+ax.set_title('K-means objective function', fontsize=20)
+plt.show()
 
 # This is how to plot an image. We ask that any images in your writeup be grayscale images, just as in this example.
 plt.figure()
-plt.imshow(pics[0].reshape(28,28), cmap='Greys_r')
+plt.imshow(KMeansClassifier.mu[0].reshape(28,28), cmap='Greys_r')
 plt.show()
 
 
 class HAC(object):
 	def __init__(self, linkage):
 		self.linkage = linkage
+
+
+
+
+
